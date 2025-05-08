f864500e451337f84e1f959b8b0f55e4d90d5af5
luan <keeganluan919@gmail.com>
2025-05-08 11:11:54 -0400

    Import do header e footer para a home

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const observer = new IntersectionObserver((entradas) => {
       }
     });
   }, {
-    threshold: 0.4
+    threshold: 0.3
   });
   
   document.querySelectorAll('.ave-box').forEach(el => {
--- templates/template-loader.js
+++ templates/template-loader.js
@@ -1,4 +1,20 @@
-document.getElementById("footer").innerHTML = fetch("templates/footer-template.html").then(res => res.text()).then(data => {
-    document.getElementById("footer").innerHTML = data;
-  });
-console.log("GLoriaa")
+// document.getElementById("footer").innerHTML = fetch("footer-template.html").then(res => res.text()).then(data => {
+//     document.getElementById("footer").innerHTML = data;
+//   });
+// console.log("GLoriaa")
+// document.getElementById("header").innerHTML = fetch("header-template.html").then(res => res.text()).then(data => {
+//     document.getElementById("header").innerHTML = data;
+// })
+
+function loadTemplateById(elemId) {
+  fetch(`templates/${elemId}-template.html`)
+      .then(res => res.text())
+      .then(data => {
+          document.getElementById(elemId).innerHTML = data;
+      })
+      .catch(err => console.error(`Erro ao carregar ${elemId}:`, err));
+}
+
+const ids = ["header", "footer"];
+
+ids.forEach(id => loadTemplateById(id));
